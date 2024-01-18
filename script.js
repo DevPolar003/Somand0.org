3785cb244a9f275a418a5df8ea619fe829dc1c83
DevPolar003 <developolar003@gmail.com>
2024-01-18 00:38:53 -0300

    Somando.org

---
+++ script.js
@@ -0,0 +1,12 @@
+
+ let num1 = Number (window.prompt ("escolha um numero"))
+  let num2 = Number (window.prompt ("escolha mais um pode ser de qualquer tamanho ")) //pode se usar number.float e number.int
+  alert (" SOMANDO_BOT: Facil demais meu consagrado 😎")
+  let soma = num1 + num2
+  window.alert (`o resultado da soma entre ${num1} e ${num2} e igual a ${soma}`) //${variavel} serve pra elaborar um codigo melhor com melhores respostas e dialogos adicionando variaveis
+
+    let nome1 = prompt ("agora me diga o seu nome")
+    let nome2 = prompt ("mais um mais um")
+    
+
+      
